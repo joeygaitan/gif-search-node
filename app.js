20622fdf2d8b223409b68eba9b4bf43d65355099
joeygaitan <joeygaitan707@gmail.com>
2020-03-31 12:04:18 -0700

    added in working tensor api with working search feature

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 // App Setup
 const app = express();
 
+const Tenor = require("tenorjs").client({
+  // Replace with your own key
+  "Key": "2HD1XTJIRK85", // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // Middleware
 const exphbs  = require('express-handlebars');
 
@@ -13,10 +20,20 @@ app.set('view engine', 'handlebars');
 // Routes
 //get all route
 app.get('/', (req, res) => {
-    // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "{ term: hey" }
+     // Handle the home page when we haven't queried yet
+     term = ""
+     if (req.query.term) {
+         term = req.query.term
+     }
+     // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+     Tenor.Search.Query(term, "10")
+         .then(response => {
+             // store the gifs we get back from the search
+             const gifs = response;
+             // pass the gifs as an object into the home page
+             res.render('home', { gifs })
+         }).catch(console.error);
   });
 
   //get one route
